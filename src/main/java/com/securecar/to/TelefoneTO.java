0c5eab1ff868abe577e8f741b5704cca4b109baf
gustavodscruz <gustavodiasdsc@gmail.com>
2024-10-17 00:17:34 -0300

    validações no telefone to junto com mensagens

--- src/main/java/com/securecar/to/TelefoneTO.java
+++ src/main/java/com/securecar/to/TelefoneTO.java
@@ -1,11 +1,26 @@
 package com.securecar.to;
 
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import jakarta.validation.valueextraction.ExtractedValue;
+
 public class TelefoneTO {
     private Long idTelefone;
+    //o char SEMPRE virá em maiúsculo, devido ao método toUppercase da classe Charater, no Setter
+    @Pattern(regexp = "^[AI]$", message = "O valor deve ser 'A' ou 'I'")
     private char status;
+    //o char SEMPRE virá em maiúsculo, devido ao método toUppercase da classe Charater, no Setter
+    @Pattern(regexp = "^[FM]$", message = "O valor deve ser 'F' ou 'M'")
     private char tipo;
+
+    @Size(min = 2, max = 2, message = "O ddi deve conter 2 caracteres")
     private int ddi;
+
+    @Size(min = 2, max = 2, message = "O ddd deve conter 2 caracteres")
     private int ddd;
+
+    @Size(min = 8, max = 9, message = "O número de telefone deve contar entre 8 e 9 dígitos numéricos")
     private int telefone;
 
     public TelefoneTO() {
@@ -23,8 +38,9 @@ public class TelefoneTO {
         return status;
     }
 
+
     public void setStatus(char status) {
-        this.status = status;
+        this.status = Character.toUpperCase(status);
     }
 
     public char getTipo() {
@@ -32,7 +48,7 @@ public class TelefoneTO {
     }
 
     public void setTipo(char tipo) {
-        this.tipo = tipo;
+        this.tipo = Character.toUpperCase(tipo);
     }
 
     public int getDdi() {
