eb7fe5147ddc3dd2ecf727afe43fb04d2306bf2f
enricodelguerra <ricodelguerra@gmail.com>
2024-10-18 10:16:47 -0300

    if e while adicionados a incidenteDAO

--- src/main/java/com/securecar/dao/IncidenteDAO.java
+++ src/main/java/com/securecar/dao/IncidenteDAO.java
@@ -3,6 +3,7 @@ package com.securecar.dao;
 import com.securecar.to.IncidenteTO;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
@@ -11,9 +12,17 @@ public class IncidenteDAO extends Repository{
         ArrayList<IncidenteTO> incidente = new ArrayList<>();
         String sql = "SELECT * FROM t_securecar_incidente";
         try(PreparedStatement ps = getConnection().prepareStatement(sql)) {
+            ResultSet rs = ps.executeQuery();
+            if (rs != null) {
+                while(){
 
+                }
+            }
         } catch (SQLException e) {
             System.out.println("Erro de SQL!" + e.getMessage());
+        } finally{
+            closeConnection();
         }
+        return incidente;
     }
 }
