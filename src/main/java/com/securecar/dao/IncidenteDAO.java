accbb7428d3018d9b1c45a95ac9e5290b3483486
enricodelguerra <ricodelguerra@gmail.com>
2024-10-22 16:17:11 -0300

    Método Find All classe IncidenteDao Fincalizado

--- src/main/java/com/securecar/dao/IncidenteDAO.java
+++ src/main/java/com/securecar/dao/IncidenteDAO.java
@@ -3,17 +3,33 @@ package com.securecar.dao;
 import com.securecar.to.IncidenteTO;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class IncidenteDAO extends Repository{
     public ArrayList<IncidenteTO>findAll(){
-        ArrayList<IncidenteTO> incidente = new ArrayList<>();
+        ArrayList<IncidenteTO> incidentes = new ArrayList<>();
         String sql = "SELECT * FROM t_securecar_incidente";
         try(PreparedStatement ps = getConnection().prepareStatement(sql)) {
-
+            ResultSet rs = ps.executeQuery();
+            if (rs != null) {
+                while (rs.next()){
+                    IncidenteTO incidente = new IncidenteTO();
+                    incidente.setIdIncidente(rs.getLong("id_incidente"));
+                    incidente.setDataIncidente(rs.getDate("dt_incidente").toLocalDate());
+                    incidente.setDesciIncidente(rs.getString("ds_incidente"));
+                    incidente.setIdUsuario(rs.getLong("id_usuario"));
+                    incidente.setIdConversa(rs.getLong("id_conversa"));
+                    incidentes.add(incidente);
+                }
+            }
         } catch (SQLException e) {
             System.out.println("Erro de SQL!" + e.getMessage());
+        } finally {
+            closeConnection();
         }
+        return incidentes;
     }
+
 }
