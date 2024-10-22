1a996e1c5be1da25cab1eca57c0d07ecdc8d8881
enricodelguerra <ricodelguerra@gmail.com>
2024-10-22 18:48:19 -0300

    Crud Incidente finalizado

--- src/main/java/com/securecar/dao/IncidenteDAO.java
+++ src/main/java/com/securecar/dao/IncidenteDAO.java
@@ -2,6 +2,7 @@ package com.securecar.dao;
 
 import com.securecar.to.IncidenteTO;
 
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -52,5 +53,49 @@ public class IncidenteDAO extends Repository{
         return incidente;
     }
 
+    public IncidenteTO save(IncidenteTO incidenteTO) {
+        String sql = "insert into T_SECURECAR_INCIDENTE (DT_INCIDENTE, DS_INCIDENTE, ID_CONVERSA) values (?, ?, ?, ?)";
+        try(PreparedStatement ps = getConnection().prepareStatement(sql)) {
+            ps.setDate(1, Date.valueOf(incidenteTO.getDataIncidente()));
+            ps.setString( 2, incidenteTO.getDesciIncidente());
+            ps.setLong(3, incidenteTO.getIdUsuario());
+            ps.setLong(4, incidenteTO.getIdConversa());
+            if (ps.executeUpdate() > 0 ){
+                return incidenteTO;
+            }
+        } catch (SQLException e) {
+            System.out.println("Erro de SQL!" + e.getMessage());
+        } finally {
+            closeConnection();
+        }
+        return null;
 
+    }
+
+    public boolean delete(Long codigo){
+        String sql = "delete T_SECURECAR_INCIDENTE where ID_INCIDENTE = ?";
+        try(PreparedStatement ps = getConnection().prepareStatement(sql)){
+            ps.setLong(1, codigo);
+            return ps.executeUpdate() > 0;
+        } catch (SQLException e) {
+            System.out.println("Erro de SQL! " + e.getMessage());
+        } finally {
+            closeConnection();
+        }
+        return false;
+    }
+
+    public IncidenteTO edit(Long id, IncidenteTO incidente) {
+        String sql = "update T_SECURECAR_INCIDENTE set DT_INCIDENTE = ?, DS_INCIDENTE = ? ";
+        try(PreparedStatement ps = getConnection().prepareStatement(sql)){
+            ps.setLong(1, incidente.getIdIncidente());
+            ps.setDate(2, Date.valueOf(incidente.getDataIncidente()));
+            ps.setString(3, incidente.getDesciIncidente());
+        } catch (SQLException e) {
+            System.out.println("Erro de SQL" + e.getMessage());
+        } finally {
+            closeConnection();
+        }
+        return null;
+    }
 }
