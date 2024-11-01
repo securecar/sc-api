00631b570222a9bbb6b0e28be3f89f130ceae093
gustavodscruz <gustavodiasdsc@gmail.com>
2024-11-01 17:32:50 -0300

    funcionando local #deploydeuruim

--- src/main/java/com/securecar/Main.java
+++ src/main/java/com/securecar/Main.java
@@ -2,7 +2,6 @@ package com.securecar;
 
 import io.github.cdimascio.dotenv.Dotenv;
 import org.glassfish.grizzly.http.server.HttpServer;
-import org.glassfish.jersey.jackson.JacksonFeature; // Import necessário
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 
@@ -11,23 +10,23 @@ import java.net.URI;
 
 /**
  * Main class.
+ *
  */
 public class Main {
     // Base URI the Grizzly HTTP server will listen on
-    public static String BASE_URI;
+    public static final String BASE_URI = "http://localhost:8080/";
 
     /**
      * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
      * @return Grizzly HTTP server.
      */
     public static HttpServer startServer() {
-        // Cria uma configuração de recursos que escaneia por recursos JAX-RS e provedores no pacote com.securecar.resource
-        final ResourceConfig rc = new ResourceConfig()
-                .packages("com.securecar.resource")
-                .register(JacksonFeature.class); // Registra o Jackson
+        // create a resource config that scans for JAX-RS resources and providers
+        // in com.securecar package
+        final ResourceConfig rc = new ResourceConfig().packages("com.securecar.resource");
 
-        // Cria e inicia uma nova instância do servidor HTTP Grizzly
-        // Expondo a aplicação Jersey no BASE_URI
+        // create and start a new instance of grizzly http server
+        // exposing the Jersey application at BASE_URI
         return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
     }
 
@@ -37,44 +36,17 @@ public class Main {
      * @throws IOException
      */
     public static void main(String[] args) throws IOException {
-        // Carrega variáveis de ambiente
-        String dbUrl = System.getenv("DB_URL");
-        String dbUsername = System.getenv("DB_USERNAME");
-        String dbPassword = System.getenv("DB_PASSWORD");
-        String dbDriver = System.getenv("DB_DRIVER");
-    
-        if (dbUrl == null || dbUsername == null || dbPassword == null || dbDriver == null) {
-            Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-            if (dbUrl == null) {
-                dbUrl = dotenv.get("DB_URL");
-            }
-            if (dbUsername == null) {
-                dbUsername = dotenv.get("DB_USERNAME");
-            }
-            if (dbPassword == null) {
-                dbPassword = dotenv.get("DB_PASSWORD");
-            }
-            if (dbDriver == null){
-                dbDriver = dotenv.get("DB_DRIVER");
-            }
-        }
-    
-        // Define propriedades do sistema
-        System.setProperty("DB_URL", dbUrl);
-        System.setProperty("DB_USERNAME", dbUsername);
-        System.setProperty("DB_PASSWORD", dbPassword);
-        System.setProperty("DB_DRIVER", dbDriver);
-    
-        // Inicia o servidor
+        Dotenv dotenv = Dotenv.load();
+
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+        System.setProperty("DB_DRIVER", dotenv.get("DB_DRIVER"));
+
         final HttpServer server = startServer();
-        System.out.println(String.format("Jersey app started at %s\nPress Ctrl+C to stop...", BASE_URI));
-    
-        // Mantém o servidor rodando até que o processo seja interrompido
-        try {
-            Thread.currentThread().join();
-        } catch (InterruptedException e) {
-            System.err.println("Servidor interrompido: " + e.getMessage());
-        }
+        System.out.println(String.format("Jersey app started with endpoints available at "
+                + "%s%nHit Ctrl-C to stop it...", BASE_URI));
+        System.in.read();
+        server.stop();
     }
 }
-
